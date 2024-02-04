557500aa170d40b1037c75d76c0f291156e18909
Najib Saad <najibsaad100@gmail.com>
2024-02-04 21:32:25 +0200

    Add files via upload

---
+++ p.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from chartit import DataPool, Chart
+
+def chartit_view(request):
+    # Data for the example
+    data = {'X': [1, 2, 3, 4, 5], 'Y': [2, 3, 5, 7, 11]}
+    data_series = DataPool(series=[data])
+
+    # Create a line chart using Django Chartit
+    chart = Chart(
+        datasource=data_series,
+        series_options=[{'options': {'type': 'line', 'stacking': False}, 'terms': {'X': ['Y']}}],
+        chart_options={'title': {'text': 'Django Chartit Line Chart'}}
+    )
+
+    context = {'chart': chart}
+    return render(request, 'chartit_template.html', context)
