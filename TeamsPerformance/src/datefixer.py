7e813c1a7c0a7d6b23a20169a26f760773f9608d
karamba <karamba@campus.technion.ac.il>
2024-02-19 19:54:34 +0200

    fix datefix algorithem

--- TeamsPerformance/src/datefixer.py
+++ TeamsPerformance/src/datefixer.py
@@ -8,11 +8,11 @@ class DateFixer:
         self.year = LAST_WIDGET_YEAR
         self.jan_pass_flag = False
     def fix_Date(self,date : str):
-            if "jan" in date.lower():
-                self.jan_pass_flag = True
-            if "jan" not in date.lower() and self.jan_pass_flag:
-                self.jan_pass_flag = False
-                self.year -= 1
-            if len(date.split()) != 3:
-                date += f" {self.year}"
-            return datetime.strptime(date, DATE_FORMAT_STR)
+        if "jan" in date.lower():
+            self.jan_pass_flag = True
+        if "jan" not in date.lower() and self.jan_pass_flag:
+            self.jan_pass_flag = False
+            self.year -= 1
+        if len(date.split()) != 3:
+            date += f" {self.year}"
+        return datetime.strptime(date, DATE_FORMAT_STR)
