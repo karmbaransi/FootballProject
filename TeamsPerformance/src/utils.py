db27b1a8c50f819be70ebb75f1870a522550fb94
karamba <karamba@campus.technion.ac.il>
2024-04-22 21:53:31 +0300

    gix8

--- TeamsPerformance/src/utils.py
+++ TeamsPerformance/src/utils.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from datetime import datetime
 from datetime import timedelta
 from TeamsPerformance.src.match import Match
@@ -163,8 +163,8 @@ def get_matches(season, sport, league, team,upcoming_date=None):
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
     print("stammmmmmmmmmmmm111111")
-    caps = DesiredCapabilities.CHROME
-    caps['loggingPrefs'] = {'browser': 'ALL'}
+    # caps = DesiredCapabilities.CHROME
+    # caps['loggingPrefs'] = {'browser': 'ALL'}
     # driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps)
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
