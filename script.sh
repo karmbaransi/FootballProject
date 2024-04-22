a2e899bd6de280a3157ed39037ed0409c74fb798
karamba <karamba@campus.technion.ac.il>
2024-04-22 21:07:02 +0300

    fix4

--- script.sh
+++ script.sh
@@ -3,4 +3,7 @@ pip install termcolor
 pip install webdriver_manager
 python manage.py makemigrations
 python manage.py migrate
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -f
+dpkg -i google-chrome-stable_current_amd64.deb
 python manage.py runserver
