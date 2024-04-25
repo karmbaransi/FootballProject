74f37a885c7bf1f0c9b3bf680fe61ee516e91f01
karamba <karamba@campus.technion.ac.il>
2024-04-25 20:55:12 +0300

    n

--- TeamsPerformance/src/utils.py
+++ TeamsPerformance/src/utils.py
@@ -162,11 +162,11 @@ def get_matches(season, sport, league, team,upcoming_date=None):
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': 'en_UK'})
 
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
     options = webdriver.ChromeOptions()
     options.headless = True
-    options.add_argument(f'user-agent={user_agent}')
+    # options.add_argument(f'user-agent={user_agent}')
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--allow-running-insecure-content')
